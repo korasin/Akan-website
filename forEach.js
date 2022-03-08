3c1a2a9a4267e96b3cee374d6cb0e112091aa858
korasin <mchemutai232@gmail.com>
2022-03-08 10:34:12 +0300

    Add files

---
+++ forEach.js
@@ -0,0 +1,8 @@
+// console.log(words = {programming: 2, fun: 5});
+// console.log(Object.keys(words));
+//  console.log(words.programming)
+// console.log(words.fun)
+//  console.log(words.nothing)
+// console.log(words.fun>words.programming)
+// console.log(var languages)
+  
